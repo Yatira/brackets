95185c0aba1969f201062cb2b25dd08d82d5f023
Yatira <irishovna@gmail.com>
2022-02-23 11:47:06 +0300

    docs: change js file

--- src/index.js
+++ src/index.js
@@ -1,35 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  function check(str, configs) {
-    const rxArr = bracketsConfig.map(getPairRx);
-    let testString = str;
-    let checkedNumber;
-    
-    do {
-      checkedNumber = testString.length;
-      testString = removeAllPairs(rxArr, testString);
-    } while(testString.length > 0 && testString.length !== checkedNumber);
-    
-    function escapeChars(string) {
-      return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
-    };
-  
-    function getPairRx(bracketsConfig) {
-      return new RegExp(`${escapeChars(bracketsConfig.join(''))}`, 'g');
-    }
-    
-    function removePair(rx, str) {
-      return str.replace(rx, '');
-    }
-    
-    function removeAllPairs(rxArr, str) {
-      rxArr.forEach(rx => { str = removePair(rx, str); });
-  
-      return str;
-    }
-  
-    return testString.length === 0;
-  }
-}
+  let brackets = bracketsConfig.join('').replace(/,/g, '');
+        let stack = [];
+    for (let bracket of str) {
+            let bracketsIndex = brackets.indexOf(bracket)
 
-console.clear();
+            if (bracketsIndex % 2 === 0) {
+                stack.push(bracketsIndex)
+                if (bracket === brackets[bracketsIndex+1]){
+                    stack.pop();
+                }
+                
+            } 
+            else {
+                if (stack.pop() !== bracketsIndex-1){
+                    return false;
+                }
+            }
+            
+        }
+        return stack.length === 0
+    
+}
